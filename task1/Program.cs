1f82bbc90753b8f23a6da87890f0376920ed0994
doomkinn-geek <doomkinn@yandex.ru>
2021-04-15 21:48:09 +0300

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Resources;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine($"{Properties.Resources.Greetings}");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            if (Properties.Resources.UserName.Trim() != "")
+            {
+                Console.WriteLine($"Имя пользователя: {Properties.Resources.UserName}");
+            }
+            else
+            {
+                Console.Write("Введите имя пользователя: ");
+                string userName = Console.ReadLine();
+                IResourceWriter writer = new ResourceWriter("myResources.resources");
+                writer.AddResource("UserName", userName);                
+            }
+            if (Properties.Resources.Age.Trim() != "")
+            {
+                Console.WriteLine($"Возраст пользователя: {Properties.Resources.Age}");
+            }
+        }
+    }
+}
