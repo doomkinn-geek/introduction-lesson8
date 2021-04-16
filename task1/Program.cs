f65c260e9d55e9bc1dab8bdb725a8ad3b40553d1
doomkinn-geek <doomkinn@yandex.ru>
2021-04-16 15:16:58 +0300

    task done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,34 +1,91 @@
 ﻿using System;
-using System.Resources;
+using System.Configuration;
 
 namespace task1
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            try
+        {            
+            if (ReadSetting("Greetings") != "")
             {
-                Console.WriteLine($"{Properties.Resources.Greetings}");
+                Console.WriteLine($"{ReadSetting("Greetings")}");
             }
-            catch(Exception e)
+            else
             {
-                Console.WriteLine(e.Message);
+                AddUpdateAppSettings("Greetings", "Приветствую тебя, дикий пользователь!");
             }
-            if (Properties.Resources.UserName.Trim() != "")
+
+            if (ReadSetting("UserName") != "")
             {
-                Console.WriteLine($"Имя пользователя: {Properties.Resources.UserName}");
+                Console.WriteLine($"Имя пользователя: {ReadSetting("UserName")}");
             }
             else
             {
                 Console.Write("Введите имя пользователя: ");
                 string userName = Console.ReadLine();
-                IResourceWriter writer = new ResourceWriter("myResources.resources");
-                writer.AddResource("UserName", userName);                
+                AddUpdateAppSettings("UserName", userName);
             }
-            if (Properties.Resources.Age.Trim() != "")
+            if (ReadSetting("Age") != "")
             {
-                Console.WriteLine($"Возраст пользователя: {Properties.Resources.Age}");
+                Console.WriteLine($"Возраст пользователя: {ReadSetting("Age")}");
+            }
+            else
+            {
+                Console.Write("Введите возраст пользователя: ");
+                string age = Console.ReadLine();
+                AddUpdateAppSettings("Age", age);
+            }
+            if (ReadSetting("Job") != "")
+            {
+                Console.WriteLine($"Род деятельности пользователя: {ReadSetting("Job")}");
+            }
+            else
+            {
+                Console.Write("Введите род деятельности пользователя: ");
+                string job = Console.ReadLine();
+                AddUpdateAppSettings("Job", job);
+            }
+        }
+
+        static string ReadSetting(string key)
+        {
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                string result = appSettings[key] ?? "";
+                return result;
+            }
+            catch (ConfigurationErrorsException)
+            {
+                return "";
+            }
+            catch(Exception)
+            {
+                return "";
+            }
+        }
+
+        static void AddUpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                //;
             }
         }
     }
